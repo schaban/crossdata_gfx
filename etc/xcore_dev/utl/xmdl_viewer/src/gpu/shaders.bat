b8598c57cf29c6cbbabee1cff6b0adb218f4df69
Sergey Chaban <sergey.chaban@gmail.com>
2019-09-21 09:52:53 +0300

    pass shaders thru validator;

--- etc/xcore_dev/utl/xmdl_viewer/src/gpu/shaders.bat
+++ etc/xcore_dev/utl/xmdl_viewer/src/gpu/shaders.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
+set GLSL=D:\Tools\Prog\VulkanSDK\1.0.54.0\Bin\glslangValidator.exe
 set SED=D:\Tools\Prog\cygwin64\bin\sed.exe
 set EXPR='s:\r::g; s:\\:\\\\:g; s:":\\":g; s:\t:\\t:g; s:.*:\t"&\\n":'
 
@@ -12,6 +13,7 @@ set FILE_LST='dir /b *.vert *.frag'
 echo /* shaders */ > %INC_FILE%
 for /f %%i in (%FILE_LST%) do (
 	echo %%i
+	%GLSL% %%i
 	set NAME=%%~ni
 	set KIND=%%~xi
 	set ID=!NAME!_!KIND:~1!
